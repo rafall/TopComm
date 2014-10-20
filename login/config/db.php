084463b4343ee7e4d935cb99d7fd7fe7651b1600
Hiroki Minami <rafael.hiroki@gmail.com>
2014-10-20 14:36:38 -0200

    a

--- login/config/db.php
+++ login/config/db.php
@@ -11,6 +11,7 @@
  * DB_USER: user for your database. the user needs to have rights for SELECT, UPDATE, DELETE and INSERT.
  * DB_PASS: the password of the above user
  */
+echo "Definindo tudo as coisas";
 define("DB_HOST", "localhost");
 define("DB_NAME", "login");
 define("DB_USER", "root");
