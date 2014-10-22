2932eea606aeea18bcec15f3d9487bf620c06259
Hiroki Minami <rafael.hiroki@gmail.com>
2014-10-22 10:20:29 -0200

    Adicionei uma descricao do site

--- login/views/not_logged_in.php
+++ login/views/not_logged_in.php
@@ -71,7 +71,17 @@
       </div><!-- /.container-fluid -->
     </nav>
       
-    
+    <div style="width: 450px;">
+        <h1>O que é o ColheJá?</h1>
+        <p>
+            O <strong>ColheJá</strong> é um site que aproxima produtores de fornecedores de serviço.<br>
+            Precisa colher seu milho mas não tem os equipamentos adequados?<br>
+            Procure aqui, e escolha a empresa mais perto de você e com o melhor preço.<br>
+            Precisa fazer o transporte da produção?<br>
+            Aqui você também encontra empresas que prestam serviço de logística para o seu negócio.<br>
+            Cadastre-se agora! É de graça!.
+        </p>
+    </div>
       <div style="width: 450px; margin: 0px auto 0px auto;">
         <h1>Sign Up Today!</h1>
         <form role="form" method="post" action="login/register.php" name="registerform">
