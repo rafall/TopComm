1ad6f22664d404817421537a9b8e8471af53eaf5
Hiroki Minami <rafael.hiroki@gmail.com>
2014-10-22 10:58:16 -0200

    Ajustando a descrição

--- login/views/not_logged_in.php
+++ login/views/not_logged_in.php
@@ -27,7 +27,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">ColheJá</a>
+          
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
@@ -48,6 +48,7 @@
               </ul>
             </li>
           </ul>
+            <a class="navbar-brand" href="#">ColheJá</a>
           <ul class="nav navbar-nav navbar-right">
               <form class="form-inline" role="form" style="padding-top:8px;" method="post" action="index.php" name="loginform">
                   <div class="form-group">
@@ -73,35 +74,39 @@
         <div style="width: 50%; float:left; ">
 
             <h1 style="text-align:center">O que é o ColheJá?</h1>
-<p style="text-align:center; font-size:16px;">
+            <p style="text-align:center; font-size:16px;">
                 O <strong>ColheJá</strong> é um site que aproxima produtores de fornecedores de serviço.<br>
                 Precisa colher seu milho mas não tem os equipamentos adequados?<br>
                 Procure aqui, e escolha a empresa mais perto de você e com o melhor preço.<br>
                 Precisa fazer o transporte da produção?<br>
                 Aqui você também encontra empresas que prestam serviço de logística para o seu negócio.<br>
                 Cadastre-se agora! É de graça!.
-</p>
+            </p>
         </div>
-          <div style="width: 50%; margin: 0px auto 0px auto; float:right;padding-right:200px;">
+          <div style="width: 50%; margin: 0px auto 0px auto; float:right;">
             <h1 style="text-align:center">Cadastre-se agora!</h1>
-            <form style="margin-left:auto;margin-right:auto;"role="form" method="post" action="login/register.php" name="registerform">
-              <div class="form-group">
-                <input type="text" class="form-control" id="login_input_username" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required placeholder="Nome">
+            <form role="form" method="post" action="login/register.php" name="registerform">
+              <div style="margin-left: auto; margin-right: auto">
+                
+                  <div class="form-group">
+                    <input type="text" class="form-control" id="login_input_username" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required placeholder="Nome">
+                  </div>
+                  <div class="form-group">
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Sobrenome">
+                  </div>
+                  <div class="form-group">
+                    <input type="email" id="login_input_email" class="form-control" name="user_email" requiredclass="form-control" placeholder="E-mail">
+                  </div>
+                  <div class="form-group">
+                    <input type="password" id="login_input_password_new" name="user_password_new" pattern=".{6,}" required autocomplete="off" class="form-control" placeholder="Senha">
+                  </div>
+                    <div class="form-group">
+                    <input type="password" id="login_input_password_repeat" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" class="form-control" placeholder="Repita a senha">
+                  </div>
+                  <br>
+                  <button type="submit" name="register" class="btn btn-primary btn-lg btn-block">Cadastrar</button>
+                  
               </div>
-              <div class="form-group">
-                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Sobrenome">
-              </div>
-              <div class="form-group">
-                <input type="email" id="login_input_email" class="form-control" name="user_email" requiredclass="form-control" placeholder="E-mail">
-              </div>
-              <div class="form-group">
-                <input type="password" id="login_input_password_new" name="user_password_new" pattern=".{6,}" required autocomplete="off" class="form-control" placeholder="Senha">
-              </div>
-                <div class="form-group">
-                <input type="password" id="login_input_password_repeat" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" class="form-control" placeholder="Repita a senha">
-              </div>
-              <br>
-              <button type="submit" name="register" class="btn btn-primary btn-lg btn-block">Cadastrar</button>
             </form>
           </div>
 
