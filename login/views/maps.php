565c721038f25dd44bd320e6df0a7bf907f5c8dd
Hiroki Minami <rafael.hiroki@gmail.com>
2014-10-30 04:48:10 -0200

    adicionar marcador no mapa

--- login/views/maps.php
+++ login/views/maps.php
@@ -22,6 +22,22 @@
         var map=new google.maps.Map(document.getElementById("googleMap")
           ,mapProp);
         }
+        
+        google.maps.event.addListener(map, 'click', function(event) {
+          placeMarker(event.latLng);
+        });
+
+        function placeMarker(location) {
+          var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+          });
+          var infowindow = new google.maps.InfoWindow({
+            content: 'Latitude: ' + location.lat() +
+            '<br>Longitude: ' + location.lng()
+          });
+          infowindow.open(map,marker);
+        }
 
         google.maps.event.addDomListener(window, 'load', initialize);
     </script>
