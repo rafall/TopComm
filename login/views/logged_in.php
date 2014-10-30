b2d534a8405bc965f48ec23ef06036a26d6a34c2
Hiroki Minami <rafael.hiroki@gmail.com>
2014-10-30 03:41:03 -0200

    Melhorias na interface, prinpalmente o a nav bar do topo.

--- login/views/logged_in.php
+++ login/views/logged_in.php
@@ -24,25 +24,16 @@
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="#">ColheJá</a>
+            <a class="navbar-brand" href="#" style="float: right;">Olá, <?php echo $_SESSION['user_name']; ?></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Início</a></li>
-            <li><a href="#">Alguma coisa</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Something<span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li><a href="#">Separated link</a></li>
-                <li class="divider"></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
+            <li class="active"><a href="#">Perfil</a></li>
+            <li><a href="#">Suas áreas</a></li>
+            <li><a href="#">Mensagens <span class="badge">42</span></a></li>
+              <li><a href="index.php?logout">Sair</a></li>
           </ul>
           <form class="navbar-form navbar-left" role="search">
             <div class="form-group">
@@ -50,25 +41,11 @@
             </div>
             <button type="submit" class="btn btn-default">Search</button>
           </form>
-          <ul class="nav navbar-nav navbar-right">
-              <li><a href="#"><span id="username"><img src="/TopComm/img/profile_default.png" class="img-circle" height="18px" width="18px"> <?php echo $_SESSION['user_name']; ?></span></a></li>
-              <li><span class="glyphicon glyphicon-comment" style="padding-top:15px;"></span></li>
-              <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-align-justify"></span><span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Configuration</a></li>
-                <li class="divider"></li>
-                <li><a href="index.php?logout">Sair</a></li>
-              </ul>
-            </li>
-          </ul>
         </div><!-- /.navbar-collapse -->
       </div><!-- /.container-fluid -->
     </nav>
       
-      <div style="width: 130px; height:450px; padding-top:0px;">
+      <div style="width: 300px; padding-top:0px;">
         <ul class="nav nav-pills nav-stacked" role="tablist">
             <li class="active"><a href="#">Início</a></li>
             <li><a href="#">Profile</a></li>
